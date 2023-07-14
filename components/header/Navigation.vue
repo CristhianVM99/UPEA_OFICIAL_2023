3d47489f2dfda503363ef9fc1b1ac72c77a0b31c
cristhian villca mamani <cristhianvillcamamani@gmail.com>
2023-07-14 14:26:59 -0400

    proyecto prefinal

---
+++ components/header/Navigation.vue
@@ -0,0 +1,72 @@
+<template>
+    <ul class="mainmenu">
+        <li class="has-droupdown"><n-link to="/">Inicio</n-link>
+            <ul class="submenu">
+                <li><n-link to="/about-us-two">Sobre Nosotros</n-link></li>
+                <li><n-link to="/contact-me">Contacto</n-link></li>                
+            </ul>
+        </li>
+        <!--<li class="has-droupdown"><n-link to="">Areas y Carreras</n-link>            
+            <ul class="mega-menu">                               
+                <li v-for="(area, id_area) in menuAreasyCarreras" :key="id_area">
+                    <h6 style="font-size: 0.7em;" class="menu-title">{{ area.area }}</h6>
+                    <ul class="submenu">
+                        <li v-for="(carrera, id_carrera) in area.carreras" :key="id_carrera">
+                            <a style="font-size: 0.7em;" :href="getLinkCarrera(carrera.nombre_simple)" target="_blank">{{ carrera.carrera }}</a>
+                        </li>                        
+                    </ul>
+                </li>
+            </ul>          
+        </li>-->
+
+        <li class="has-droupdown"><n-link to="">Unidades Administrativas</n-link>
+            <ul class="submenu">
+                <li><a href="https://vicerrectorado.upea.bo/l">vicerectorado</a></li>
+                <li><a href="https://www.sie.upea.bo/l">unidad de sistemas de informacion y estadistica</a></li>
+                <li><a href="https://registrosadmisiones.upea.bo/">registros y admiciones</a></li>
+            </ul>                    
+        </li>
+
+        <li class="has-droupdown"><n-link to="">Avisos</n-link>
+            <ul class="submenu">
+                <li><n-link to="/convocatorias/publicaciones">publicaciones</n-link></li>
+                <li><n-link to="/convocatorias/servicios">servicios</n-link></li>                
+                <li><n-link to="/convocatorias/gacetas">gacetas</n-link></li>
+                <li><n-link to="/convocatorias/auditorias">auditorias</n-link></li>
+                <li><n-link to="/convocatorias/eventos">eventos</n-link></li>
+                <li><n-link to="/convocatorias/videos">videos</n-link></li>                                
+            </ul>
+        </li>
+        <li class="has-droupdown"><n-link to="">Mas</n-link>
+            <ul class="submenu">
+                <li><a href="http://repositorio.upea.bo/" target="_blank">repositorio institucional</a></li>
+                <li><a href="https://disbedc.upea.bo/" target="_blank">disbec</a></li>
+                <li><a href="http://mibiblioteca.upea.bo/" target="_blank">mi biblioteca upea</a></li>
+                <li><a href="https://fm100.upea.bo/" target="_blank">radio upea</a></li>
+                <li><a href="https://tv.upea.bo/" target="_blank">tv upea</a></li>
+            </ul>
+        </li>
+    </ul>
+</template>
+<script>
+    import { useInstitucionStore } from '@/stores/store'
+    export default{
+        data() {
+            return {
+                menuAreasyCarreras: useInstitucionStore().menuAreasyCarreras,
+                linksCarreras: useInstitucionStore().linksCarreras,
+            }
+        },
+        methods: {
+            getLinkCarrera(links){
+                let link = ""
+                this.linksCarreras.forEach(element =>{        
+                    if(element.carrera == links){
+                        link = element.link
+                    }
+                })
+                return link
+            }
+        },
+    }
+</script>
---
+++ components/instructor/InstructorTwo.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="edu-team-grid team-style-2">
+        <div class="inner">
+            <div class="thumbnail-wrap">
+                <div class="thumbnail">
+                    <n-link to="/instructor/instructor-profile">
+                        <img :src="url_api + '/InstitucionUpea/Autoridad/' + autoridad.foto_autoridad" alt="" />
+                    </n-link>
+                </div>
+                <ul class="team-share-info">
+                    <!--<li v-for="(social, key) in instructor.socialLinks" :key="key">
+                        <a :aria-label="social.name" :href="social.url">
+                            <i :class="social.iconName"></i>
+                        </a>
+                    </li>-->
+                    <li><a :href="autoridad.facebook_autoridad" target="_blank" class="color-fb"><i class="icon-facebook"></i></a></li>                         
+                    <li><a :href="autoridad.twiter_autoridad" target="_blank" class="color-twitter"><i class="icon-twitter"></i></a></li>
+                    <li><a :href="autoridad.celular_autoridad" target="_blank" class="color-yt"><i class="icon-youtube"></i></a></li>
+                </ul>
+            </div>
+            <div class="content">
+                <h5 class="title">
+                    <n-link to="/instructor/instructor-profile">{{ autoridad.nombre_autoridad }}</n-link>
+                </h5>
+                <span class="designation">{{ autoridad.cargo_autoridad }}</span>
+                <!--<p>Descripcion</p>-->
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['autoridad'],
+        data() {
+            return {
+                url_api: process.env.APP_ROOT_API
+            }
+        },
+    }
+</script>
